5bd8db081e0deee58660cfca8ba45bb235927081
juicefire24 <46361030+juicefire24@users.noreply.github.com>
2019-02-05 20:23:52 -0600

    Update main.py

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,6 +1,13 @@
 import csv
 import os
 
+# Declare Variables
+#total_votes = 0
+#khan_votes = 0
+#correy_votes = 0
+#li_votes = 0
+#otooley_votes = 0
+
 def output_data(vTotal):
     tot = 0
     outList = []
