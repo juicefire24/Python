882175316b47454b07df9cb18f95f1f253b535a6
jarrett campbell <jarrett.jd1@gmail.com>
2019-02-05 19:55:55 -0600

    Python Work

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,96 +1,46 @@
-# Import dependencies
-import os
 import csv
+import os
 
-#File to load
-election_budget_data=os.path.join("..","pyPoll","election_data.csv")
+def output_data(vTotal):
+    tot = 0
+    outList = []
+    key_max = max(vTotal.keys(), key=(lambda k: vTotal[k])) # get's the key of the largest value in dict
+    for votes in vTotal.keys():
+        tot = tot + vTotal[votes]
+    for per in vTotal.keys():
+        p = '{0:.3f}'.format(vTotal[per] / tot * 100) #format to get trailing zero's
+        inStr = str(per) + ": " + str(p) + "% (" + str(vTotal[per]) + ")"
+        outList.append(inStr)
+    lStr = max(outList, key = len)
+    outFile = open("election_results.txt", 'w')
+    outFile.write("Election Results" + "\n")
+    print("Election Results")
+    outFile.write("-" * len(lStr) + "\n")
+    print("-" * len(lStr))
+    outFile.write("Total Votes: " + str(tot) + "\n")
+    print("Total Votes: " + str(tot))
+    outFile.write("-" * len(lStr) + "\n")
+    print("-" * len(lStr))
+    for line in outList:
+        outFile.write(line + "\n")
+        print(line)
+    outFile.write("-" * len(lStr) + "\n")
+    print("-" * len(lStr))
+    outFile.write("Winner: " + str(key_max) + "\n")
+    print("Winner: " + str(key_max))
+    outFile.write("-" * len(lStr) + "\n")
+    print("-" * len(lStr))
+    return
+        
 
-# Declare Variables 
-total_votes = 0 
-khan_votes = 0
-correy_votes = 0
-li_votes = 0
-otooley_votes = 0
-
-# Open csv in default read mode with context manager
-with open(csv_file_path,newline="", encoding="utf-8") as elections:
-
-    # Store data under the csvreader variable
-    csvreader = csv.reader(elections,delimiter=",") 
-
-    # Skip the header so we iterate through the actual values
-    header = next(csvreader)     
-
-    # Iterate through each row in the csv
-    for row in csvreader: 
-
-        # Count the unique Voter ID's and store in variable  called total_votes
-        total_votes +=1
-
-        # We have four candidates if the name is found, count the times it appears and store in a list
-        # We can use this values in our percent vote calculation in the print statements
-        if row[2] == "Khan": 
-            khan_votes +=1
-        elif row[2] == "Correy":
-            correy_votes +=1
-        elif row[2] == "Li": 
-            li_votes +=1
-        elif row[2] == "O'Tooley":
-            otooley_votes +=1
-
- # To find the winner we want to make a dictionary out of the two lists we previously created 
-candidates = ["Khan", "Correy", "Li","O'Tooley"]
-votes = [khan_votes, correy_votes,li_votes,otooley_votes]
-
-# We zip them together the list of candidate(key) and the total votes(value)
-# Return the winner using a max function of the dictionary 
-dict_candidates_and_votes = dict(zip(candidates,votes))
-key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
-
-# Print a the summary of the analysis
-khan_percent = (khan_votes/total_votes) *100
-correy_percent = (correy_votes/total_votes) * 100
-li_percent = (li_votes/total_votes)* 100
-otooley_percent = (otooley_votes/total_votes) * 100
-
-# Print the summary table
-print(f"Election Results")
-print(f"----------------------------")
-print(f"Total Votes: {total_votes}")
-print(f"----------------------------")
-print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
-print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
-print(f"Li: {li_percent:.3f}% ({li_votes})")
-print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
-print(f"----------------------------")
-print(f"Winner: {key}")
-print(f"----------------------------")
-
-# Output files
-# Assign output file location and with the pathlib library
-output_file = Path("python-challenge", "PyPoll", "Election_Results_Summary.txt")
-
-with open(output_file,"w") as file:
-
-# Write methods to print to Elections_Results_Summary 
-    file.write(f"Election Results")
-    file.write("\n")
-    file.write(f"----------------------------")
-    file.write("\n")
-    file.write(f"Total Votes: {total_votes}")
-    file.write("\n")
-    file.write(f"----------------------------")
-    file.write("\n")
-    file.write(f"Khan: {khan_percent:.3f}% ({khan_votes})")
-    file.write("\n")
-    file.write(f"Correy: {correy_percent:.3f}% ({correy_votes})")
-    file.write("\n")
-    file.write(f"Li: {li_percent:.3f}% ({li_votes})")
-    file.write("\n")
-    file.write(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
-    file.write("\n")
-    file.write(f"----------------------------")
-    file.write("\n")
-    file.write(f"Winner: {key}")
-    file.write("\n")
-    file.write(f"----------------------------")
+csvPath = os.path.join("election_data.csv")
+voteTotals = {}
+with open(csvPath, newline = '') as csvFile:
+    csvReader = csv.reader(csvFile, delimiter = ",")
+    next(csvReader, None)
+    for row in csvReader:
+        if (str(row[2]) in voteTotals):
+            voteTotals[str(row[2])] = voteTotals[row[2]] + 1
+        else:
+            voteTotals[str(row[2])] = 1
+output_data(voteTotals)
